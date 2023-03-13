759c7e22ee1f20852aa3ca392c43fad55ec47c49
matstn <matstn@stud.ntnu.no>
2023-03-13 15:05:30 +0100

    #1 storing changes, close to finished

---
+++ core/src/com/mygdx/game/states/State.java
@@ -0,0 +1,22 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class State {
+    protected GameStateManager gsm;
+    OrthographicCamera cam;
+    protected State(GameStateManager gsm){
+        this.gsm = gsm;
+        cam = new OrthographicCamera();
+
+
+    }
+
+
+    protected abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
+    public abstract void dispose();
+}
+
