2f475e88a8c3a21027e65937bca6ad4bb21c07c2
Julie Lundberg Suter <julielsu@stud.ntnu.no>
2023-03-20 20:50:55 +0100

    #20 game over state created

---
+++ core/src/com/mygdx/game/states/GameOverState.java
@@ -0,0 +1,60 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.MyGdxGame;
+
+public class GameOverState extends State{
+
+    private Texture background;
+    private Texture title;
+    private static int titleOffset = -160;
+    private Texture menuBtn;
+    private static int menuBtnOffset = -250;
+    private Vector3 touchPos;
+
+    protected GameOverState(GameStateManager gsm) {
+        super(gsm);
+        cam.setToOrtho(false, MyGdxGame.WIDTH, MyGdxGame.HEIGHT);
+        background = new Texture("cobraCombatBG.png");
+        title = new Texture("gameover.png");
+        menuBtn = new Texture("menu.png");
+        touchPos = new Vector3();
+    }
+
+    @Override
+    protected void handleInput() {
+        if (Gdx.input.justTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            cam.unproject(touchPos);
+
+            if(touchPos.x > MyGdxGame.WIDTH/2 - (menuBtn.getWidth()/2) && touchPos.x < MyGdxGame.WIDTH/2 + (menuBtn.getWidth()/2))
+                if (touchPos.y > cam.position.y + menuBtnOffset && touchPos.y < cam.position.y + menuBtnOffset + menuBtn.getHeight()){
+                    gsm.set(new GameMenuState(gsm));
+                }
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+        sb.draw(background, 0,0);
+        sb.draw(menuBtn, MyGdxGame.WIDTH/2 - (menuBtn.getWidth()/2),  cam.position.y + menuBtnOffset);
+        sb.draw(title, MyGdxGame.WIDTH/2 - (title.getWidth()/2), MyGdxGame.HEIGHT +titleOffset);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        title.dispose();
+    }
+}
