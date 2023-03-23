e66e0a6f7c4f7381a62499484ab272065ed0a32e
Erland Lie Amundgård <erlandla@stud.ntnu.no>
2023-03-23 11:45:48 +0100

    Moved API to GameStateManger; cleanup in MyGDXGame

--- core/src/com/mygdx/game/states/GamePlayState.java
+++ core/src/com/mygdx/game/states/GamePlayState.java
@@ -47,7 +47,7 @@ public class GamePlayState extends State {
             deltaTime = deltaTime % MyGdxGame.GAMESPEED;
 
             player.move();
-            MyGdxGame.API.sendPos(player.getPlayerData());
+            this.gsm.getApi().sendPos(player.getPlayerData());
         }
         if (deltaTime2 >= MyGdxGame.GAMESPEED*4) {
             deltaTime2 = deltaTime2 % MyGdxGame.GAMESPEED*4;
